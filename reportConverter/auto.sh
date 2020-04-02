fbb6a99b049e0669eda2d5f2dc2c0186f8428e7e
Ritter, Thomas <thomas.ritter@de.zuken.com>
2020-04-02 08:59:12 +0200

    Logging extended

--- reportConverter/auto.sh
+++ reportConverter/auto.sh
@@ -4,7 +4,7 @@ SCRIPTPATH=`dirname $SCRIPT`
 
 set -e
 echo "Files in report raw directory"
-ls "$1"
+ls "$1/raw"
 
 for f in $(find "$SCRIPTPATH" -maxdepth 1 -type f -name "cov.sh"); do #"*.sh"
     if [ $(basename $0) = $(basename $f) ]; then
